4266ab2b34f60c886ca9d4962d13236db04ffd82
drewn-0 <g4curb3r21@gmail.com>
2025-06-18 23:23:24 +0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,23 @@
 import { NavLink } from 'react-router-dom';
+import mapIcon from '../public/icons/map.svg';
+import graffitiListIcon from '../public/icons/graffiti_list.svg';
+import reportIcon from '../public/icons/report.svg';
+import addNewIcon from '../public/icons/add_new.svg';
 
 const Nav = () => {
   return (
     <nav className="navbar">
       <NavLink to="/" className={({ isActive }) => `nav-button ${isActive ? 'active' : ''}`}>
-        <img src='/icons/map.svg' alt="Map" className="icon" />
+        <img src={mapIcon} alt="Map" className="icon" />
       </NavLink>
       <NavLink to="/all-graffiti" className={({ isActive }) => `nav-button ${isActive ? 'active' : ''}`}>
-        <img src='/icons/graffiti_list.svg' alt="Graffiti List" className="icon" />
+        <img src={graffitiListIcon} alt="Graffiti List" className="icon" />
       </NavLink>
       <NavLink to="/report" className={({ isActive }) => `nav-button ${isActive ? 'active' : ''}`}>
-        <img src='/icons/report.svg' alt="Report" className="icon" />
+        <img src={reportIcon} alt="Report" className="icon" />
       </NavLink>
       <NavLink to="/add-graffiti" className={({ isActive }) => `nav-button ${isActive ? 'active' : ''}`}>
-        <img src='/icons/add_new.svg' alt="Add Graffiti" className="icon" />
+        <img src={addNewIcon} alt="Add Graffiti" className="icon" />
       </NavLink>
     </nav>
   );
