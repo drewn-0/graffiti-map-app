6f92e6ebe157cb96809161961129bd9b207f3006
drewn-0 <g4curb3r21@gmail.com>
2025-06-18 22:36:14 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
 import { Link } from 'react-router-dom';
-import settingsIcon from '/icons/options.svg';
-import profileIcon from '/icons/profile.svg';
 
 const Header = () => {
   return (
@@ -11,10 +9,10 @@ const Header = () => {
       </div>
       <div className="header-right">
         <Link to="/settings" className="header-button">
-          <img src={settingsIcon} alt="Settings" className="icon" />
+          <img src='/icons/options.svg' alt="Settings" className="icon" />
         </Link>
         <Link to="/profile" className="header-button profile">
-          <img src={profileIcon} alt="Profile" className="icon" />
+          <img src='/icons/profile.svg' alt="Profile" className="icon" />
         </Link>
       </div>
     </header>
